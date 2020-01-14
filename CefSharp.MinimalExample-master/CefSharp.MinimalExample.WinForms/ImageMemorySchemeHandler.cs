ffdaeb0881c6ca85e39e5ebba44afb97c95bda76
Jose Maria Peña <jpespartero@gmail.com>
2020-01-14 11:12:19 +0100

    Example for loading in memory images

---
+++ CefSharp.MinimalExample-master/CefSharp.MinimalExample.WinForms/ImageMemorySchemeHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefSharp.MinimalExample.WinForms
+{
+    /// <summary>
+    /// Carga una imagen desde memoria
+    /// </summary>
+    public class ImageMemorySchemeHandler : ResourceHandler
+    {
+        ///<summary>
+        /// 
+        /// </summary>
+        public ImageMemorySchemeHandler()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Search in the dictionary if exist and provide in the request
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public override CefReturnValue ProcessRequestAsync(IRequest request, ICallback callback)
+        {
+            var uri = new Uri(request.Url);
+            string url = System.Net.WebUtility.UrlDecode(request.Url);
+
+            int localIndex = url.IndexOf("local/");
+            string imageId = url.Substring(localIndex + 6);
+
+            Bitmap image;
+            ImageMemorySchemeHandlerFactory.Images.TryGetValue(imageId, out image);
+
+            if (image != null)
+            {
+                Stream = new MemoryStream();
+                image.Save(Stream, ImageFormat.Png);
+                MimeType = GetMimeType(".png");
+            }
+
+            callback.Continue();
+            return CefReturnValue.Continue;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ImageMemorySchemeHandlerFactory : ISchemeHandlerFactory
+    {
+        // Custom scheme
+        public const string SchemeName = "memoryImage";
+
+        // Static dictionary with image resources
+        public static Dictionary<string, Bitmap> Images = new Dictionary<string, Bitmap>();
+
+        public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
+        {
+            return new ImageMemorySchemeHandler();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="image"></param>
+        public static void AddImage(string id, Bitmap image)
+        {
+            if (!Images.ContainsKey(id))
+            {
+                Images.Add(id, image);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Clear()
+        {
+            Images.Clear();
+        }
+
+    }
+
+}
---
+++ CefSharp.MinimalExample-master/CefSharp.MinimalExample.WinForms/Program.cs
@@ -0,0 +1,49 @@
+﻿// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using CefSharp.WinForms;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CefSharp.MinimalExample.WinForms
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            //For Windows 7 and above, best to include relevant app.manifest entries as well
+            Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Example of setting a command line argument
+            //Enables WebRTC
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+
+            // Custom scheme for memory images
+            CefCustomScheme cefImagesInMemoryScheme = new CefCustomScheme
+            {
+                SchemeName = ImageMemorySchemeHandlerFactory.SchemeName,
+                SchemeHandlerFactory = new ImageMemorySchemeHandlerFactory()
+            };
+            settings.RegisterScheme(cefImagesInMemoryScheme);
+
+            Bitmap image = new Bitmap("test.png");
+            ImageMemorySchemeHandlerFactory.AddImage("test.png", image);
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+            var browser = new BrowserForm();
+            Application.Run(browser);
+        }
+    }
+}
